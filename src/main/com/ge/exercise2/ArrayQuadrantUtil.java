1f7ee3d4bd1945ab4f0b46e8a8341395efe269a4
Dharmendra Kumar Awasthi <dharmendrakawasthi@gmail.com>
2020-03-04 16:20:59 +0530

    Adding exercise2

--- src/main/com/ge/exercise2/ArrayQuadrantUtil.java
+++ src/main/com/ge/exercise2/ArrayQuadrantUtil.java
@@ -3,16 +3,158 @@ package com.ge.exercise2;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ArrayQuadrantUtil {
-    private static final Logger logger = LogManager.getLogger(ArrayQuadrantUtil.class);
+public class ArrayQuadrantUtil<T> {
 
-    char[][] data;
+	private static final Logger logger = LogManager.getLogger(ArrayQuadrantUtil.class);
 
-    public ArrayQuadrantUtil(char[][] data) {
-        this.data = data;
-    }
+	T[][] data;
 
-    public char[] getQuadrantValues(int row, int column) {
-        return null;
-    }
+	public ArrayQuadrantUtil(T[][] data) {
+		this.data = data;
+	}
+
+	public T[] getQuadrantValues(int row, int column) {
+
+//		Get middle number of the rows and columns
+		
+		int midRow = data[0].length / 2;
+		int midCol = data.length / 2;
+
+//		Generic array to store the data
+		T[] e = (T[]) new Object[midRow + midCol];
+
+		int len = e.length;
+
+		int maxRow = 0;
+		int maxCol = 0;
+		int minRow = 0;
+		int minCol = 0;
+
+
+		if (row < midRow && column < midCol) {				//		Check for first Quadrant
+
+//			initialize the veriables according to the first quadrant	
+			maxRow = midRow - 1;
+			maxCol = midCol - 1;
+			minRow = 0;
+			minCol = 0;
+
+			while (len > minCol) {
+
+				e[len - 1] = data[maxRow][maxCol];
+
+				if (maxCol > 0) {
+					maxCol--;
+				} else {
+					maxCol = midCol - 1;
+					maxRow--;
+				}
+
+				--len;
+			}
+
+		} else if (row < midRow && column > midCol) {       // Check for second Quadrant
+
+//			initialize the veriables according to the second quadrant	
+
+			maxRow = midRow - 1;
+			maxCol = data[0].length - 1;
+			minRow = 0;
+			minCol = midCol;
+
+			while (len > 0) {
+
+				e[len - 1] = data[maxRow][maxCol];
+
+				if (maxCol > minCol) {
+					maxCol--;
+				} else {
+					maxCol = data[0].length - 1;
+					maxRow--;
+				}
+
+				--len;
+			}
+
+		} else if (row > midRow && column < midCol) {		// Check for third Quadrant
+
+//			initialize the veriables according to the third quadrant	
+
+			maxRow = data[0].length - 1;
+			maxCol = midCol - 1;
+			minRow = midRow;
+			minCol = 0;
+
+			while (len > minCol) {
+
+				e[len - 1] = data[maxRow][maxCol];
+
+				if (maxCol > 0) {
+					maxCol--;
+				} else {
+					maxCol = midCol - 1;
+					maxRow--;
+				}
+
+				--len;
+			}
+
+		} else if (row > midRow && column > midCol) {			// Check for fourth Quadrant
+
+//			initialize the veriables according to the fourth quadrant	
+
+			maxRow = data[0].length - 1;
+			maxCol = data.length - 1;
+			minRow = midRow;
+			minCol = midCol;
+
+			while (len > 0) {
+
+				e[len - 1] = data[maxRow][maxCol];
+				System.out.println(data[maxRow][maxCol]);
+				if (maxCol > minCol) {
+					maxCol--;
+				} else {
+					maxCol = data[0].length - 1;
+					maxRow--;
+				}
+
+				--len;
+			}
+		}
+
+		return e;
+	}
+
+	public T[] getRowCol(String rowCol, int index) {
+
+		int midCol = data[0].length / 2;
+		int midRow = data.length / 2;
+
+		T[] e = (T[]) new Object[midRow + midCol];
+
+		if (rowCol.equals("row")) {
+
+			int rowNum = index - 1;
+			int len = data[0].length - 1;
+
+			while (len >= 0) {
+				e[len] = data[rowNum][len];
+				len--;
+			}
+		} else if (rowCol.equals("col")) {
+
+			int colNum = index - 1;
+			int len = data.length - 1;
+
+			while (len >= 0) {
+				e[len] = data[len][colNum];
+				System.out.println(data[len][colNum]);
+				len--;
+			}
+
+		}
+
+		return e;
+	}
 }
--- src/test/com/ge/exercise2/ArrayQuadrantUtilTest.java
+++ src/test/com/ge/exercise2/ArrayQuadrantUtilTest.java
@@ -9,17 +9,54 @@ public class ArrayQuadrantUtilTest {
 
     @Test
     public void getQuadrantValuesTest() {
-        char[][] data = {
+        Character[][] data = {
                 {'a', 'b', 'c', 'd'},
                 {'e', 'f', 'g', 'h'},
                 {'i', 'j', 'k', 'l'},
                 {'m', 'n', 'o', 'p'}
         };
 
-        ArrayQuadrantUtil util = new ArrayQuadrantUtil(data);
+        ArrayQuadrantUtil<Character> util = new ArrayQuadrantUtil<Character>(data);
+        
         Assume.assumeNotNull(util.getQuadrantValues(0, 0));
+        Assume.assumeNotNull(util.getQuadrantValues(1, 3));
+        Assume.assumeNotNull(util.getQuadrantValues(3, 1));
+        Assume.assumeNotNull(util.getQuadrantValues(2, 3));
 
-        char[] expectedResult = {'a', 'b', 'e', 'f'};
-        assertArrayEquals(expectedResult, util.getQuadrantValues(0, 0));
+        Character[] expectedResult1 = {'a', 'b', 'e', 'f'};
+        assertArrayEquals(expectedResult1, util.getQuadrantValues(0, 0));
+        
+        Character[] expectedResult2 = {'c', 'd', 'g', 'h'};
+        assertArrayEquals(expectedResult1, util.getQuadrantValues(2, 3));
+        
+        Character[] expectedResult3 = {'i', 'j', 'm', 'n'};
+        assertArrayEquals(expectedResult1, util.getQuadrantValues(2, 3));
+        
+        Character[] expectedResult4 = {'k', 'l', 'o', 'p'};
+        assertArrayEquals(expectedResult1, util.getQuadrantValues(2, 3));
+
     }
+    
+    @Test
+    public void getRowColTest() {
+    	
+    	Character[][] data = {
+                {'a', 'b', 'c', 'd'},
+                {'e', 'f', 'g', 'h'},
+                {'i', 'j', 'k', 'l'},
+                {'m', 'n', 'o', 'p'}
+        };
+
+        ArrayQuadrantUtil<Character> util = new ArrayQuadrantUtil<Character>(data);
+        
+        Assume.assumeNotNull(util.getRowCol("row", 2));
+        Assume.assumeNotNull(util.getRowCol("col", 3));
+        
+        Character[] expectedRow = {'i','j','k','l'};
+        Character[] expectedCol = {'d','h','l','p'};
+    	
+    }
+    
+    
+    
 }
