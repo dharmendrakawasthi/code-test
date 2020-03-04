67bc05d96172279e47cfdb8a4baf19fdcaa36241
Dharmendra Awasthi <dharmendrakawasthi@gmail.com>
2020-03-04 23:43:59 +0530

    Created new Engine class GE9x

--- src/main/com/ge/exercise4/GE9x.java
+++ src/main/com/ge/exercise4/GE9x.java
@@ -1,5 +1,77 @@
 package com.ge.exercise4;
 
-public class GE9x {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class GE9x {
+	    
+    private static final Logger logger = LogManager.getLogger(GE9x.class);
+
+    private static final String ENGINE_MODEL = GENx.class.getSimpleName();
+    private final String serialNumber;
+    
+	private final int	maxNumRebuilds = 5;
+	private final double flightHoursBeforeRebuild = 30_000;
+	private final double dryWeight = 15_505;
+	private final double wetWeight = 15_900;
+	private final double takeoffThrust = 100_000;
+	
+	private double flightHours;
+	private int numRebuilds;
+	    
+	public GE9x(String serialNumber, double flightHours, int numRebuilds) {
+        this.serialNumber = serialNumber;
+        this.flightHours = flightHours;
+        this.numRebuilds = numRebuilds;
+    }
+
+    public GE9x(String serialNumber, double flightHours) {
+        this(serialNumber, flightHours, 0);
+    }
+
+    public GE9x(String serialNumber) {
+        this(serialNumber, 0.0);
+    }
+
+    public double getFlightHours() {
+        return flightHours;
+    }
+
+    public void setFlightHours(double flightHours) {
+        this.flightHours = flightHours;
+    }
+    
+	public int getMaxNumRebuilds() {
+		return maxNumRebuilds;
+	}
+	public double getFlightHoursBeforeRebuild() {
+		return flightHoursBeforeRebuild;
+	}
+	public double getDryWeight() {
+		return dryWeight;
+	}
+	public double getWetWeight() {
+		return wetWeight;
+	}
+	public double getTakeoffThrust() {
+		return takeoffThrust;
+	}
+	
+	public double thrustToWeightRatio() {
+//      return takeoffThrust / wetWeight;
+  	  return takeoffThrust / dryWeight;
+  }
+
+  public String toString() {
+      return ENGINE_MODEL + " SN: " + serialNumber;
+  }
+  
+  public double timeToRebuild() {
+  	return flightHoursBeforeRebuild - flightHours;
+  }
+  
+  public double serviceLifeLeft() {
+  	
+  	return ((maxNumRebuilds - numRebuilds)*flightHoursBeforeRebuild) - flightHours;
+  }
 }
